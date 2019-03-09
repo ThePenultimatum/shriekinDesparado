02be2c89d6fe875c25e35c7d95a64b434636e840
Mark Dyehouse <markdyehouse2019@u.northwestern.edu>
2019-03-10 01:53:23 +0900

    Added start and ending to processing reddit files

--- processReddit.py
+++ processReddit.py
@@ -7,6 +7,14 @@ allFS = os.listdir(".")
 
 fs = ["anotherSplitAFcd"]
 abcs = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+start = """<url>http://www.ted.com/talks/david_gallo_on_life_in_the_deep_oceans</url>
+<keywords>talks, TED Conference, animals, geology, life, oceans, science, submarine, technology</keywords>
+<speaker>David Gallo</speaker>
+<talkid>343</talkid>
+<title>David Gallo: Life in the deep oceans</title>
+<description>TED Talk Subtitles and Transcript: With vibrant video clips captured by submarines, David Gallo takes us to some of Earth's darkest, most violent, toxic and beautiful habitats, the valleys and volcanic ridges of the oceans' depths, where life is bizarre, resilient and shockingly abundant.</description>"""
+ending = """<reviewer href="http://www.ted.com/profiles/490889">Elena Montrasio</reviewer>
+<translator href="http://www.ted.com/profiles/258692">Geoffrey Chen</translator>"""
 
 #treeSoFar = AnyNode(id="root")
 #parentIds = set()
@@ -62,10 +70,10 @@ def takeIdsAndText(fs, columns):
 				responses.append(v)
 				#idsDoneAsResponses.append(k)
 		oprompts = open("prompts.txt", 'a')
-		oprompts.write("\n".join(prompts))
+		oprompts.write(start + ("\n".join(prompts)) + ending)
 		oprompts.close()
 		oresponses = open("responses.txt", 'a')
-		oresponses.write("\n".join(responses))
+		oresponses.write(start + ("\n".join(responses)) + ending)
 		oresponses.close()
 		i += 1
 
