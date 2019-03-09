7dd190b8fb0b802c675518bb3190d930cf58395c
Mark Dyehouse <markdyehouse2019@u.northwestern.edu>
2019-03-10 03:36:09 +0900

    most recent update to reddit processing

--- processReddit.py
+++ processReddit.py
@@ -7,20 +7,8 @@ allFS = os.listdir(".")
 
 fs = ["anotherSplitAFcd"]
 abcs = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-start = """<url>http://www.ted.com/talks/david_gallo_on_life_in_the_deep_oceans</url>
-<keywords>talks, TED Conference, animals, geology, life, oceans, science, submarine, technology</keywords>
-<speaker>David Gallo</speaker>
-<talkid>343</talkid>
-<title>David Gallo: Life in the deep oceans</title>
-<description>TED Talk Subtitles and Transcript: With vibrant video clips captured by submarines, David Gallo takes us to some of Earth's darkest, most violent, toxic and beautiful habitats, the valleys and volcanic ridges of the oceans' depths, where life is bizarre, resilient and shockingly abundant.</description>"""
-ending = """<reviewer href="http://www.ted.com/profiles/490889">Elena Montrasio</reviewer>
-<translator href="http://www.ted.com/profiles/258692">Geoffrey Chen</translator>"""
 
-#treeSoFar = AnyNode(id="root")
-#parentIds = set()
-#ids = set()
 promptsById = dict()
-#idsDoneAsResponses = dict()
 
 def getRelevantVals(lineJson, fields):
 	return [lineJson[i] for i in fields]
@@ -41,39 +29,34 @@ def takeIdsAndText(fs, columns):
 		responsesByParentId = dict()
 		for l in ls:
 			lineJson = json.loads(l)
-			firstBody = str(filter(lambda c: c in printable, lineJson['body']))
+			firstBody = str(filter(lambda c: c in printable, lineJson['body'])).strip()
 			body = firstBody.replace("\n", " ")
 			tmp = firstBody.split(" ")
 			idval = str(filter(lambda c: c in printable, lineJson['id']))
 			pid = str(filter(lambda c: c in printable, lineJson['parent_id'][3:]))
-			if ((len(body) > 0) and (body[0] in abcs) and ("[removed]" not in body) and (body != "") and (not body.isspace())):
+			if ((len(body) > 0) and (body[0] in abcs) and ("[removed]" not in body) and (body not in ["", " ", "\n", "\b", "\t", "\r\n"]) and (not body.isspace())):
 				if (len(tmp) > 40):
 					body = " ".join(tmp[:40])
+				elif ("\n" in body):
+					body = (body.split("\n"))[0]
 
 				promptsById[idval] = body
 				if (pid != "null"):
 					responsesByParentId[pid] = body
-			#author = lineJson['author']
-			#parentIds.add(lineJson['parent_id'][3:])
-			#ids.add(lineJson['id'])
-			#resLine = filter(lambda c: c in printable ,",".join(getRelevantVals(lineJson, columns)))
-			#res.append(resLine)
-		#text = "\n".join(res) #filter(lambda c: c in printable, "\n".join(res))
-		#o.close()
-		#newO = open(("new2_" + f), 'w')
-		#newO.write(text)
-		#newO.close()
-		#allIds = promptsById.keys()
+
+		#iterating through responses because responses are what define a response - prompt pair. there could be more prompts overall than responses
+		# so we only want to count the pairs
 		for k, v in responsesByParentId.iteritems():
 			if k in promptsById:
-				prompts.append(promptsById[k])
-				responses.append(v)
-				#idsDoneAsResponses.append(k)
+				pb = promptsById[k]
+				if (len(pb) > 1) and (len(v) > 1):
+					prompts.append(pb)
+					responses.append(v)
 		oprompts = open("prompts.txt", 'a')
-		oprompts.write(start + ("\n".join(prompts)) + ending)
+		oprompts.write("\n".join(prompts))
 		oprompts.close()
 		oresponses = open("responses.txt", 'a')
-		oresponses.write(start + ("\n".join(responses)) + ending)
+		oresponses.write("\n".join(responses))
 		oresponses.close()
 		i += 1
 
